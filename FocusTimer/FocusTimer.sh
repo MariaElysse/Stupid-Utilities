8057127fbce342bd4bd8312f1890de437266c774
Jeremy Neptune <jerenept@gmail.com>
2016-11-28 22:23:49 -0800

    made FocusTimer's dialog more readable, using mm:ss instead of fractions of minutes, which are stupid

--- FocusTimer/FocusTimer.sh
+++ FocusTimer/FocusTimer.sh
@@ -10,19 +10,22 @@ while true; do
 		for i in $(seq 1 100); do 
 			sleep 15
 			echo $i
-			mins_rem="$(echo "scale=2; 25-(0.05*$i)" | bc -l | sed 's/^\./0./')" 
-			echo "# $mins_rem mins work remaining"
+			mins_rem="$(echo "(1500-(15*$i)) / 60 " | bc )"
+			secs_rem="$(echo "(1500-(15*$i)) % 60 " | bc )" 
+			echo "# $mins_rem:$secs_rem work remaining"
 		done | zenity --auto-close --progress --no-cancel --text="You are working"
 	else 
 		notify-send "Take a break!"
 		mplayer -really-quiet /usr/share/sounds/gnome/default/alerts/glass.ogg
 		cp default.action_noblock /etc/privoxy/default.action
+		service privoxy restart
 		let time=25
 		for i in $(seq 1 100); do
 			sleep 3
 			echo $i
-			mins_rem="$(echo "scale=2; 5-(0.05*$i)" | bc -l | sed 's/^\./0./')"
-			echo "# $mins_rem mins break remaining" 
+			mins_rem="$(echo "(300-(3*$i)) / 60 " | bc )"
+			secs_rem="$(echo "(300-(3*$i)) % 60 " | bc )" 
+			echo "# $mins_rem:$secs_rem break remaining" 
 		done | zenity --progress --auto-close --no-cancel --text="You are on a break"
 	fi
 done 
